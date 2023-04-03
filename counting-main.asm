afb7af54fb3cd985d4f46fa82fd9793345dec3e6
ardleypacheco <89606723+ardleypacheco@users.noreply.github.com>
2023-04-03 14:32:21 -0500

    Finished counting-main.asm
    Added additional comments for register conventions, output for char

--- counting-main.asm
+++ counting-main.asm
@@ -1,13 +1,21 @@
+# Ardley Pacheco
+# Dakota Leslie
 # Teamwork Lab
-# Program searches through a string for a specific character based on user input
+# Program searches through a string for a specific character based on user input and 
+# prints how many times the character appears the string
 # Psuedocode:
 #	str = input("Please enter a string ")
 #	char = input("Please enter a character to search for ")
 #	countOccur(str, char)
 #	print("Search string = " + str)
-#	print("We found the character " + char + count + " times")
+#	print("We found the character " + char + " " + count + " times")
 #	exit
 # 
+# Register Conventions:
+#	$a0 = string to be passed to the countOccur function
+#	$a1 = character to be passed to countOccur function
+#	#v0 = contains number of times char appears in string
+#	#s0 = used for saving values temporarily
 .text
 main:
 	#str = input("Please enter a string ")
@@ -36,6 +44,7 @@ main:
 	move $a0, $s0
 	move $a1, $v0
 	
+	# call the function
 	jal countOccur
 	
 	# save the value of count into memory
@@ -51,9 +60,18 @@ main:
 	la $a0, input
 	syscall
 	
-	# print("We found the character " + char + count + " times")
+	# print("We found the character " + char + " " + count + " times")
 	li $v0, 4
 	la $a0, count_output1
+	syscall
+	
+	# Print the character 
+	la $a0,($a1)
+	li $v0,11
+	syscall  
+	
+	li $v0, 4
+	la $a0, space
 	syscall
 	
 	# print the count
@@ -75,6 +93,7 @@ string_prompt: "Please enter a string: "
 char_prompt: "Please enter a character to search for: "
 str_output: "\nSearch string = "
 count_output1: "We found the character "
+space: " "
 count_output2: " times"
 input: .space 81
 inputSize: .word 80
