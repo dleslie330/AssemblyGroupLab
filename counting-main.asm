63d1e5da0209b0005e6a0196ce46c88c85c2c07f
ardleypacheco <89606723+ardleypacheco@users.noreply.github.com>
2023-04-03 13:50:52 -0500

    Added output for string and count

--- counting-main.asm
+++ counting-main.asm
@@ -3,7 +3,7 @@
 # Psuedocode:
 #	str = input("Please enter a string ")
 #	char = input("Please enter a character to search for ")
-#	CountOccur(str, char)
+#	countOccur(str, char)
 #	print("Search string = " + str)
 #	print("We found the character " + char + count + " times")
 #	exit
@@ -21,6 +21,8 @@ main:
 	lw $a1, inputSize
 	syscall
 	
+	move $s0, $a0
+	
 	#char = input("Please enter a character to search for ")
 	li $v0, 4
 	la $a0, char_prompt
@@ -30,10 +32,40 @@ main:
 	li $v0, 12
 	syscall
 	
+	# shift the values into their parameter form
+	move $a0, $s0
 	move $a1, $v0
 	
-	jal CountOccur
+	jal countOccur
 	
+	# save the value of count into memory
+	sw $v0, count
+	
+	# print("Search string = " + str)
+	li $v0, 4
+	la $a0, str_output
+	syscall
+	
+	# print str
+	li $v0, 4
+	la $a0, input
+	syscall
+	
+	# print("We found the character " + char + count + " times")
+	li $v0, 4
+	la $a0, count_output1
+	syscall
+	
+	# print the count
+	lw $a0, count
+	li $v0, 1
+	syscall
+	
+	# print final output
+	li $v0, 4
+	la $a0, count_output2
+	syscall
+
 	#exit the program
 	li $v0, 10
 	syscall
@@ -41,7 +73,10 @@ main:
 .asciiz 
 string_prompt: "Please enter a string: "
 char_prompt: "Please enter a character to search for: "
+str_output: "\nSearch string = "
+count_output1: "We found the character "
+count_output2: " times"
 input: .space 81
 inputSize: .word 80
-.word 
-count: 0
+count: .word 0
+.include "count-occur.asm"
